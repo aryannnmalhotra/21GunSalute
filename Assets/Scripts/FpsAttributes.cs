53f2d35051c9dc50042d22c3c1ba6f73caa12fd9
Amalhotra29 <aryan.malhotra2000@yahoo.in>
2020-12-13 20:29:56 +0530

    Minor fix

--- Assets/Scripts/FpsAttributes.cs
+++ Assets/Scripts/FpsAttributes.cs
@@ -6,17 +6,14 @@ using UnityEngine.SceneManagement;
 
 public class FpsAttributes : MonoBehaviour
 {
-    private float alphaValue;
     private HealthSystem healthSystem;
     public Controller ARController;
-    public Image Black;
     public AudioSource UIAudioPlayer;
     public AudioSource BGAudio;
     public AudioClip youDied;
     public static bool IsAlive;
     void Start()
     {
-        alphaValue = 0;
         IsAlive = true;
         healthSystem = GetComponent<HealthSystem>();
     }
@@ -33,13 +30,7 @@ public class FpsAttributes : MonoBehaviour
             ARController.Scroller.enabled = false;
             BGAudio.Stop();
             UIAudioPlayer.PlayOneShot(youDied);
-            Invoke("LoadScreen", 4);
-        }
-        if (!IsAlive)
-        {
-            Color color = Black.color;
-            color.a = Mathf.Clamp(alphaValue + Time.deltaTime / 4, 0, 0.5f); 
-            Black.color = color;
+            Invoke("LoadScreen", 3);
         }
     }
 }
