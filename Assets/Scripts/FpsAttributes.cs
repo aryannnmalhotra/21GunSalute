053ad6cce213c4c38e294e1e5fb3bef820dcd09c
Amalhotra29 <aryan.malhotra2000@yahoo.in>
2020-12-13 20:20:24 +0530

    Improved Death Handling/Scene Transitions

--- Assets/Scripts/FpsAttributes.cs
+++ Assets/Scripts/FpsAttributes.cs
@@ -1,23 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class FpsAttributes : MonoBehaviour
 {
+    private float alphaValue;
     private HealthSystem healthSystem;
+    public Controller ARController;
+    public Image Black;
+    public AudioSource UIAudioPlayer;
+    public AudioSource BGAudio;
+    public AudioClip youDied;
     public static bool IsAlive;
     void Start()
     {
+        alphaValue = 0;
         IsAlive = true;
         healthSystem = GetComponent<HealthSystem>();
+    }
+    void LoadScreen()
+    {
+        SceneManager.LoadScene("LaunchScene");
     }
     void Update()
     {
         if (healthSystem.GetHealth() == 0 && IsAlive)
         {
             IsAlive = false;
-            SceneManager.LoadScene("LaunchScene"); 
+            ARController.Scroller.SetIndex(-1);
+            ARController.Scroller.enabled = false;
+            BGAudio.Stop();
+            UIAudioPlayer.PlayOneShot(youDied);
+            Invoke("LoadScreen", 4);
+        }
+        if (!IsAlive)
+        {
+            Color color = Black.color;
+            color.a = Mathf.Clamp(alphaValue + Time.deltaTime / 4, 0, 0.5f); 
+            Black.color = color;
         }
     }
 }
